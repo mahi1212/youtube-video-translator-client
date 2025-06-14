9c71c22a9b0e07418802426385a826f903fac07b
mahi1212 <mahi121.mr@gmail.com>
2025-06-14 13:16:28 +0600

    blog uncommented

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function ContentCreator() {
               <div className="absolute right-0 top-1/2 transform -translate-y-1/2 w-px h-8 bg-gray-300/50 md:block hidden" />
             </TabsTrigger>
             
-            <TabsTrigger
+            {/* <TabsTrigger
               value="blog"
               className="relative px-6 py-4 rounded-xl font-medium text-base transition-[transform,background-color,box-shadow] duration-300 ease-in-out -mt-1
                          data-[state=active]:bg-gradient-to-br data-[state=active]:from-blue-400 data-[state=active]:to-blue-600 
@@ -39,7 +39,7 @@ export default function ContentCreator() {
               <Edit className="w-5 h-5 mr-2" />
               Blog Post
               <div className="absolute right-0 top-1/2 transform -translate-y-1/2 w-px h-8 bg-gray-300/50 md:block hidden" />
-            </TabsTrigger>
+            </TabsTrigger> */}
             
             <TabsTrigger
               value="social"
